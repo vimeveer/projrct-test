dcbc3ac5b1b038fe177fcf117a6b68b7f6ca9742
code <123@qq.com>
2022-09-12 23:25:16 +0800

    11

---
+++ src/components/12.js
@@ -0,0 +1,63 @@
+const data =  [
+  {
+    title: '第一层a',
+    index:1,
+    children: [{
+      title: '第二层a',
+      index:1,
+      children: [{
+        title: '第三层a',
+        index:1,
+        children: [{
+          title: '第四层a',
+          index:1,
+          children: [{
+            title: '第五层a',
+            index:1
+          }]
+        }]
+      }
+      ]
+    }
+    ]
+  },
+  {
+    title: '第一层b',
+    index:1,
+    children: [{
+      title: '第二层b',
+      index:1,
+      children: [{
+        title: '第三层b',
+        index:1,
+        children: [{
+          title: '第五层b',
+          index:1,
+        }]
+      }]
+    }
+    ]
+  },
+  {
+    title: '第一层c',
+    index:1,
+    children: [{
+      title: '第二层c',
+      index:1,
+      children: [{
+        title: '第五层c',
+        index:1,
+      }]
+    }
+    ]
+  },
+  {
+    title: '第一层d',
+    index:1,
+    children: [{
+      title: '第五层c',
+      index:1,
+    }
+    ]
+  }
+  ]
